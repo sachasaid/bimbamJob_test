a0b4445c4268b657bab6c936113c6bd0cc42bc54
Sacha <sacha@MacBook-Pro-de-Sacha.local>
2021-01-21 11:40:36 +0100

    final algo  + add README for differente step of the project

--- index.js
+++ index.js
@@ -1,98 +1,174 @@
-const lineReader = require('line-reader');
-const regexL = /^[DGA]*$/gm
-const regexNNL = /^\d\s\d\s[NEWS]$/gm
-const regexNN = /^\d\s?\d$/gm;
+//1ere partie => utilisation de regex pour parse le file
 
-lineReader.eachLine('./test.txt', function(line) {
-    console.log(line);
-    if (line.match(regexL)) {
-        console.log('regex L')
-    } else if (line.match(regexNNL)) {
-        console.log('regex N N L')
-    } else if (line.match(regexNN)) {
-        console.log('regex N N')
-    } else {
-        console.log('this instruction is not allowed')
-    }
-});
+// const lineReader = require('line-reader');
+// const regexInstruction = /^[DGA]*$/gm
+// const regexOrientation = /^\d\s\d\s[NEWS]$/gm
+// const regexMap = /^\d\s?\d$/gm;
 
-function direction(c, x, y) {
-    switch (c) {
-        case 'G':
-            console.log('x - 1')
-            break;
-        case 'D':
-            console.log('x + 1')
-            break;
-        case 'A':
-            console.log('y + 1')
-            break;
-        default:
-            break;
-    }
-}
+// lineReader.eachLine('./test.txt', function(line) {
+//     console.log(line);
+//     if (line.match(regexInstruction)) {
+//         const instrucArray = Array.from(line)
+//         console.log(instrucArray)
+//         console.log('regex L')
+//     } else if (line.match(regexOrientation)) {
+//         const [x, y, orientation] = line.split(' ')
+//         console.log(x, y, orientation)
+//         console.log('regex N N L')
+//     } else if (line.match(regexMap)) {
+//         const [mapX, mapY] = line.split(' ')
+//         console.log(mapX, mapY)
+//         console.log('regex N N')
+//     } else {
+//         console.log('this instruction is not allowed')
+//     }
+// });
 
-const cols = 5
-const rows = 5
-const tab = []
 
-function start() {
+// Je n'ai pas réussi a faire marcher mon parser avec mon algo 
+
+function Road() {
+    // Initialisation de mon Array
     const array = [];
-    const height = 5;
-    const width = 5;
-    for (let i = 0; i < height; i++) {
+
+    //initialisation de la taille de ma map
+    const rows = 5;
+    const cols = 5;
+
+    //Creation d'un double tableau
+    for (let i = 0; i < rows; i++) {
         const subArray = [];
-        for (let j = 0; j < width; j++) {
+        for (let j = 0; j < cols; j++) {
             subArray.push(-1);
         }
         array.push(subArray);
     }
 
-    console.log(array);
+    // Initialisation de la position de ma tondeuse
     const x = 4;
     const y = 4;
+
+    // const x = 2;
+    // const y = 2;
+
+    // Creation d'un objet pour numériser mes differentes orientations
     const orientationToNumeric = {
-        'N': 0,
-        'E': 1,
-        'S': 2,
-        'W': 3,
+        N: 0,
+        E: 1,
+        S: 2,
+        W: 3,
     };
 
-    const orientation = orientationToNumeric['S'];
+    // Verification que ma tondeuse ne soit pas hors de la map
+    const checkMovePosition = (posX, posY) => {
+        let newPosX = posX < 0 ? 0 : posX >= cols ? cols - 1 : posX;
+        let newPosY = posY < 0 ? 0 : posY >= rows ? rows - 1 : posY;
+        return [newPosX, newPosY];
+    };
 
-    array[y - 1][x - 1] = orientation;
+    //Initialisation de mon orientation avec mon tableau orientationToNumeric
+    const orientation = orientationToNumeric["S"];
+    // const orientation = orientationToNumeric["N"];
 
-    console.log(array);
+    //Creation array pour l'orientation
+    const orientationArray = ["N", "E", "S", "W"];
 
-    const orientationArray = ['N', 'E', 'S', 'W']
-
+    //Tableau pour rendre numérique la direction de mon objet
     const directionObject = {
-        'D': 1,
-        'G': -1,
+        D: 1,
+        G: -1,
     };
 
-    const directions = ['G', 'A', 'D', 'D', 'A', 'A', 'G', 'A', 'D', 'A', 'A'];
-    const direction = 'G';
+    //initialisation de mes directions
+    const directions = ["G", "A", "D", "D", "A", "A", "G", "A", "D", "A", "A"];
+    //const directions = ["A", "A", "D", "G", "G", "D", "A", "D", "G", "A"];
 
+    const posX = x - 1;
+
+    // Rendre absolu ma position Y car ma map doit commencer en bas à gauche
+    const posY = Math.abs(y - rows);
+
+    array[posY][posX] = orientation;
+
+    //Creation de mes 2 variables finales
+    let finalPosX = posX;
+    let finalPosY = posY;
+
+    //Debut de ma boucle
     for (let i = 0; i < directions.length; i++) {
-        const isOrientation = directions[i] !== 'A';
+        // Verification si c'est une orientation
+        const isOrientation = directions[i] !== "A";
         if (isOrientation) {
             const orientationModifier = directionObject[directions[i]];
-            let finalOrientation = array[y - 1][x - 1] + orientationModifier
+            let finalOrientation = array[finalPosY][finalPosX] + orientationModifier;
             if (finalOrientation >= orientationArray.length) {
                 finalOrientation = 0;
             } else if (finalOrientation < 0) {
                 finalOrientation = orientationArray.length - 1;
             }
-            array[y - 1][x - 1] = finalOrientation;
+            array[finalPosY][finalPosX] = finalOrientation;
+        } else {
+            //set de la position 
+            const currentOrientation = array[finalPosY][finalPosX];
+            switch (currentOrientation) {
+                // Si c'est 'S'
+                case 0:
+                    {
+                        const [newPosX, newPosY] = checkMovePosition(
+                            finalPosX,
+                            finalPosY - 1
+                        );
+                        array[newPosY][newPosX] = currentOrientation;
+                        finalPosX = newPosX;
+                        finalPosY = newPosY;
+                        break;
+                    }
+                    // Si c'est 'E'
+                case 1:
+                    {
+                        const [newPosX, newPosY] = checkMovePosition(
+                            finalPosX + 1,
+                            finalPosY
+                        );
+                        array[newPosY][newPosX] = currentOrientation;
+                        finalPosX = newPosX;
+                        finalPosY = newPosY;
+                        break;
+                    }
+                    // Si c'est 'N'
+                case 2:
+                    {
+                        const [newPosX, newPosY] = checkMovePosition(
+                            finalPosX,
+                            finalPosY + 1
+                        );
+                        array[newPosY][newPosX] = currentOrientation;
+                        finalPosX = newPosX;
+                        finalPosY = newPosY;
+                        break;
+                    }
+                    // Si c'est 'W'
+                case 3:
+                    {
+                        const [newPosX, newPosY] = checkMovePosition(
+                            finalPosX - 1,
+                            finalPosY
+                        );
+                        array[newPosY][newPosX] = currentOrientation;
+                        finalPosX = newPosX;
+                        finalPosY = newPosY;
+                        break;
+                    }
+            }
         }
-
     }
-
-    array[y - 1][x - 1] = array[y - 1][x - 1] + directionObject[direction];
-
-    console.log(array, orientationArray[array[y - 1][x - 1]]);
+    //Affichage de mes resultats
+    console.log(
+        finalPosX + 1,
+        Math.abs(finalPosY - rows),
+        orientationArray[array[finalPosY][finalPosX]]
+    );
 
 }
 
-start()
+Road()
